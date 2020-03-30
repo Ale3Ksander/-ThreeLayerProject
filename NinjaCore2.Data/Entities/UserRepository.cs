128fad6ef29f5b8e2d2118011252802e84b61191
Ale3Ksander <ksander.l27@gmail.com>
2020-03-30 15:17:31 +0300

    Message

---
+++ NinjaCore2.Data/Entities/UserRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NinjaCore2.Data.DataContext;
+using NinjaCore2.Data.Repositories;
+using NinjaCore2.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NinjaCore2.Data.Entities
+{
+    public class UserRepository : IUserRepository
+    {
+        private UserContext context;
+
+        public UserRepository(UserContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<User> GetUserList()
+        {
+            return context.Users;
+        }
+
+        public User GetUser(int id)
+        {
+            return context.Users.Find(id);
+        }
+
+        public void Create(User user)
+        {
+            context.Users.Add(user);
+        }
+
+        public void Update(User user)
+        {
+            context.Entry(user).State = EntityState.Modified;
+        }
+
+        public void Delete(int id)
+        {
+            User user = context.Users.Find(id);
+            if (user != null)
+                context.Users.Remove(user);
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+    }
+}
---
+++ NinjaCore2.Data/Repositories/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using NinjaCore2.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NinjaCore2.Data.Repositories
+{
+    public interface IUserRepository
+    {
+        IEnumerable<User> GetUserList();
+        User GetUser(int id);
+        void Create(User user);
+        void Update(User user);
+        void Delete(int id);
+        void Save();
+    }
+}
